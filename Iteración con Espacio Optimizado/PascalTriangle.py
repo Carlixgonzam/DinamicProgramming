7cef0bf04aa7f2b2df6b6ec3e15feb29c41f48fd
Carla Gonzalez <carlafernanda006@gmail.com>
2025-06-17 20:37:54 -0500

    new exercise of pascal's problem

---
+++ Iteración con Espacio Optimizado/PascalTriangle.py
@@ -0,0 +1,12 @@
+def generate(numRows: int)->list:
+    resp= []
+    for n in range(numRows):
+        row= [1]*(n+1)
+        for k in range(1, n):
+            row[k]= resp[n-1][k-1] + resp[n-1][k]
+        
+        resp.append(row)
+    return resp
+
+numRows=5
+print(generate)
